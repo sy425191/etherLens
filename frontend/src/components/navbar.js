250af7a4ac5cfce3d3ba935758d55d581e2277c6
saurabh yadav <sy425191@gmail.com>
2024-01-28 16:50:16 +0530

    Navbar Completed

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,17 +1,42 @@
+import { useEffect, useRef } from "react";
+
 const Navbar = () => {
+  const inpurRef = useRef(null);
+  const handler = (event) => {
+    const isCtrlK = event.ctrlKey && event.key === "k";
+    if (isCtrlK) {
+      event.preventDefault();
+      inpurRef.current.focus();
+    }
+  };
+
+  const setEventListener = () => {
+    window.addEventListener("keydown", handler);
+  };
+
+  useEffect(() => {
+    setEventListener();
+
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, []);
+
   return (
     <div className="w-full h-16 flex justify-between items-center bg-slate-800">
-      <div className=""> Block </div>
-      <div className="w-1/2">
+      <div className="px-3 font-semibold"> BlockExpo </div>
+      <div className="w-1/2 flex">
         <input
           type="text"
-          className="w-full h-12 rounded-md bg-slate-900 text-white px-4 outline-none"
-          placeholder="Ctrl + K to search"
+          className="w-full h-12 bg-slate-900 text-white px-4 outline-none rounded-l-xl"
+          placeholder="Ctrl + K to search Address | Transaction"
+          ref={inpurRef}
         />
+        <div className="h-12 flex justify-center items-center px-4 bg-slate-900 rounded-r-xl border-2 border-slate-500 cursor-pointer hover:bg-slate-500 transition duration-300 ease-in-out">
+            Search
+        </div>
       </div>
-      <div className="flex justify-end items-center text-white pr-4">
-        🔔
-      </div>
+      <div className="flex justify-end items-center text-white pr-4">🔔</div>
     </div>
   );
 };
